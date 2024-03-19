2be3ec568ecc9d7e0f226874359d73267ec6683a
Mathew <brzoza.mathew@gmail.com>
2024-03-19 09:37:37 +0000

    'lesson-form-handling-with-server-actions'

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import NewTodoForm from "@/components/ToDoList";
-
-const getData = async () => {
-  await new Promise((res) => setTimeout(() => res(0), 2000));
-  return { data: [1, 2, 3] };
-};
-
 const Home = async () => {
-  const data = await getData();
-  console.log("data", data);
-  return (
-    <div>
-      <NewTodoForm />
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default Home;
